9548ff286b723701883ac193b4e5afc8396a5504
Antoine Augusti <antoine.augusti@gmail.com>
2020-07-20 14:32:18 -0400

    Split in 2 templates

--- udata_schema/views.py
+++ udata_schema/views.py
@@ -1,28 +1,61 @@
+from urllib.parse import urlencode
+
 from flask import Blueprint
 
 from udata import theme
-
 from udata.frontend import template_hook
 
 blueprint = Blueprint('schema', __name__, template_folder='templates')
 
 
-def has_schema(ctx):
-    resource = ctx['resource']
-    return resource.schema is not None
+def validata_url(resource):
+    base = "https://validata.etalab.studio/table-schema"
+
+    query = urlencode({
+        "input": "url",
+        "schema_name": f"schema-datagouvfr.{resource.schema}",
+        "url": resource.url,
+    })
+
+    return f"{base}?{query}"
 
 
-@template_hook('dataset.resource.card.extra-buttons', when=has_schema)
+def resource_has_schema(ctx):
+    return 'resource' in ctx and ctx['resource'].schema
+
+
+def dataset_has_schema(ctx):
+    if 'dataset' not in ctx:
+        return False
+    return any([r.schema is not None for r in ctx['dataset'].resources])
+
+
+@template_hook('dataset.resource.card.extra-buttons', when=resource_has_schema)
 def resource_schema_details(ctx):
     resource = ctx['resource']
 
-    validation_url = f"https://validata.etalab.studio/table-schema?input=url&schema_name=schema-datagouvfr.{resource.schema}&url={resource.url}"
-    documentation_url = f"https://schema.data.gouv.fr/{resource.schema}/latest.html"
-
     return theme.render(
-        'schema-details.html',
+        'button.html',
         resource=resource,
         id=str(resource.id).replace('-', ''),
-        validation_url=validation_url,
-        documentation_url=documentation_url,
+    )
+
+
+@template_hook('base.modals', when=dataset_has_schema)
+def resource_schema_modal(ctx):
+    dataset = ctx['dataset']
+
+    documentation_urls = {}
+    validation_urls = {}
+    for resource in [r for r in dataset.resources if r.schema]:
+        validation_urls[resource.id] = validata_url(resource)
+        documentation_urls[resource.id] = (
+            f"https://schema.data.gouv.fr/{resource.schema}/latest.html"
+        )
+
+    return theme.render(
+        'modal.html',
+        dataset=dataset,
+        documentation_urls=documentation_urls,
+        validation_urls=validation_urls,
     )
