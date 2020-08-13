dbb45613dfaf5b392da1d5a2a7b5d7e4040f4bbe
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-08-12 21:50:47 -0400

    Add task to set the IRVE schema (#3)
    * Add task to set the IRVE schema
    
    * Find resource by UUID
    
    * PR comments

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This udata plugin provides an integration with [schema.data.gouv.fr](https://sch
 
 ## Usage
 
-Install the plugin package in your udata environement:
+Install the plugin package in your udata environment:
 
 ```bash
 pip install udata-schema-gouvfr
@@ -27,3 +27,13 @@ PLUGINS = ['schema-gouvfr']
 You can control this plugin behavior with the following `udata.cfg` parameters:
 
 - **`SCHEMA_GOUVFR_VALIDATA_URL`**: the URL to your [Validata](https://validata.fr/) instance (without trailing slash). **ex:** `https://validata.etalab.studio`
+- **`SCHEMA_GOUVFR_IRVE_STABLE_RESOURCE_URL`**: the permalink to the consolidated [IRVE dataset](irve-dataset)'s latest resource. **ex:** `https://www.data.gouv.fr/fr/datasets/r/50625621-18bd-43cb-8fde-6b8c24bdabb3`
+
+## Jobs
+
+This plugin declares jobs.
+
+- **`udata job run set-irve-schemas`**: sets the `schema` attribute of resources that are consolidated as part of [the national IRVE dataset](irve-dataset)
+
+
+[irve-dataset]: https://www.data.gouv.fr/fr/datasets/fichier-consolide-des-bornes-de-recharge-pour-vehicules-electriques/
---
+++ udata_schema_gouvfr/tasks.py
@@ -0,0 +1,68 @@
+import re
+import csv
+import uuid
+import codecs
+import logging
+
+import requests
+
+from flask import current_app
+from udata.tasks import job
+from udata.core.dataset.models import get_resource, ResourceSchema
+
+log = logging.getLogger(__name__)
+
+
+IRVE_SCHEMA = "etalab/schema-irve"
+
+
+def find_sources():
+    """
+    Find all (dataset, resource) listed in the consolidated IRVE resource
+    """
+    response = requests.get(current_app.config.get('SCHEMA_GOUVFR_IRVE_STABLE_RESOURCE_URL'))
+    response.raise_for_status()
+
+    sources = set()
+    reader = csv.DictReader(
+        codecs.iterdecode(response.iter_lines(), "utf-8"), delimiter=";"
+    )
+    for record in reader:
+        sources.add(record["source"])
+
+    return sources
+
+
+def ensure_irve_schema_exists():
+    """
+    Ensure that the IRVE schema exists
+    """
+    if not any([el["id"] == IRVE_SCHEMA for el in ResourceSchema.objects()]):
+        raise ValueError(f"{IRVE_SCHEMA} is not a valid schema")
+
+
+def set_irve_schemas(sources):
+    """
+    Update a list of resources by setting the schema attribute to
+    the IRVE schema
+    """
+    ensure_irve_schema_exists()
+
+    pattern = re.compile(
+        r"^https://www\.data\.gouv\.fr/fr/datasets/([\S]+)/#resource-([\S]+)$"
+    )
+
+    log.info(f'Preparing to set the {IRVE_SCHEMA} schema on {len(sources)} resources')
+    for source in sources:
+        dataset_slug, resource_id = re.match(pattern, source).groups()
+        resource = get_resource(uuid.UUID(resource_id))
+        if not resource:
+            log.warning(f'Cannot find a resource with ID {resource_id}. It may have been deleted.')
+        resource.schema = IRVE_SCHEMA
+        resource.save()
+        log.info(f'Set the {IRVE_SCHEMA} on resource {resource_id} from dataset {dataset_slug}')
+
+
+@job("set-irve-schemas")
+def run_set_irve_schemas(self):
+    set_irve_schemas(find_sources())
