a0ba712dab736a890d429a3b8428d94d24a0524b
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-05-10 12:09:12 +0200

    Remove Validation section for schema other than tableschema + Correct validation URL

--- udata_schema_gouvfr/views.py
+++ udata_schema_gouvfr/views.py
@@ -5,20 +5,52 @@ from flask import Blueprint, current_app
 from udata import theme
 from udata.frontend import template_hook
 
+import requests
+
 blueprint = Blueprint('schema', __name__, template_folder='templates')
 
 
-def validata_url(resource):
+def validata_url(resource,schema_url=None):
     base = current_app.config.get('SCHEMA_GOUVFR_VALIDATA_URL')
-
-    query = urlencode({
-        "input": "url",
-        "schema_name": f"schema-datagouvfr.{resource.schema}",
-        "url": resource.url,
-    })
+    if(schema_url):
+        query = urlencode({
+            "input": "url",
+            "schema_url": schema_url,
+            "url": resource.url,
+        })
+    else:
+        query = urlencode({
+            "input": "url",
+            "schema_name": f"schema-datagouvfr.{resource.schema['name']}",
+            "url": resource.url,
+        })
 
     return f"{base}/table-schema?{query}"
 
+def isTableSchema(schemas,current_schema):
+    try:
+        for schema in schemas:
+            if((schema['name'] == current_schema) & (schema['schema_type'] == 'tableschema')): return True
+        return False
+    except:
+        return False
+
+def getSchemaUrl(schemas,current_schema,current_schema_version):
+    try:
+        for schema in schemas:
+            if(schema['name'] == current_schema): 
+                for version in schema['versions']:
+                    if(version['version_name'] == current_schema_version): return version['schema_url']
+        return
+    except:
+        return
+
+def loadCatalog(): 
+    r = requests.get('https://schema.data.gouv.fr/schemas/schemas.json')
+    try:
+        return r.json()['schemas']
+    except:
+        return
 
 def resource_has_schema(ctx):
     return ctx.get('resource') and ctx['resource'].schema
@@ -44,13 +76,23 @@ def resource_schema_details(ctx):
 @template_hook('base.modals', when=dataset_has_schema)
 def resource_schema_modal(ctx):
     dataset = ctx['dataset']
+    schemas = loadCatalog()
 
     documentation_urls = {}
+    authorize_validation = {}
     validation_urls = {}
     for resource in [r for r in dataset.resources if r.schema]:
-        validation_urls[resource.id] = validata_url(resource)
+
+        authorize_validation[resource.id] = isTableSchema(schemas,resource.schema['name'])
+
+        if(authorize_validation[resource.id]):
+            schema_url = None
+            if("version" in resource.schema):
+                schema_url = getSchemaUrl(schemas,resource.schema['name'],resource.schema['version'])
+            validation_urls[resource.id] = validata_url(resource, schema_url)
+
         documentation_urls[resource.id] = (
-            f"https://schema.data.gouv.fr/{resource.schema}/latest.html"
+            f"https://schema.data.gouv.fr/{resource.schema['name']}/latest.html"
         )
 
     return theme.render(
@@ -58,4 +100,5 @@ def resource_schema_modal(ctx):
         dataset=dataset,
         documentation_urls=documentation_urls,
         validation_urls=validation_urls,
+        authorize_validation=authorize_validation,
     )
